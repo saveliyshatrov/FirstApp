978b0b4a21c4f5373a944d09bb9316b5de2f5366
time_guy <31863650+saveliyshatrov@users.noreply.github.com>
2020-07-25 21:40:47 +0300

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -18,34 +18,67 @@ class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 0, 1),
       appBar: AppBar(
-        title: Text('Page #1'),
-        backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+        title: Text(
+          'Page #1',
+          style: TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 1)
+          ),
+          ),
+        backgroundColor: Colors.orange,
       ),
       body: Container(
-        child: Center(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.orange, Color.fromRGBO(0, 0, 0, 1)]
+          )
+        ),
+        padding: EdgeInsets.only(top:7),
+        child: ListView(
+          children: <Widget>[
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              shadowColor: Colors.orange,
+              color: Colors.white,
+              child: ListTile(
+                onTap: () {
+                  debugPrint("I was clicked");
+                },
+                leading: Icon(
+                  Icons.accessibility,
+                  size: 56,
+                ),
+                title: Text('Two-line ListTile'),
+                subtitle: Text('Here is a second line'),
+                trailing: Icon(Icons.desktop_mac)
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton.extended(
         label: Text(
           "Next Page",
-          style: TextStyle(
-            color: Color.fromRGBO(0, 0, 0, 1)
-            ),
-          ),
+          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1)),
+        ),
         icon: Icon(
           Icons.arrow_forward,
           color: Color.fromRGBO(0, 0, 0, 1),
-        ), 
+        ),
         backgroundColor: Colors.orange,
         onPressed: () {
-              Navigator.push(
-                context,
-                CupertinoPageRoute(builder: (context) => PageTwo()),
-              );
-          }
-        ),
+          Navigator.push(
+            context,
+            CupertinoPageRoute(builder: (context) => PageTwo()),
+          );
+        }
+      ),
     );
   }
 }
