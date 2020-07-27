1c9528de2b0bc4e852dfebf9c0c4ffc58cc665c1
time_guy <31863650+saveliyshatrov@users.noreply.github.com>
2020-07-27 17:24:10 +0300

    added carousel

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +14,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// page one
+Widget createElemForCarousel(
+    String name, Color backgroundColor, Color fontColor) {
+  return Builder(
+    builder: (BuildContext context) {
+      return new InkWell(
+          onTap: () {
+            debugPrint("$name");
+          },
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: backgroundColor.withOpacity(0.6),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 2), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Center(
+                  child: Text(
+                '$name',
+                style: TextStyle(fontSize: 30.0, color: fontColor),
+              ))));
+    },
+  );
+}
+
 class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,64 +53,70 @@ class PageOne extends StatelessWidget {
       backgroundColor: Color.fromRGBO(0, 0, 0, 1),
       appBar: AppBar(
         title: Text(
-          'Page #1',
-          style: TextStyle(
-            color: Color.fromRGBO(0, 0, 0, 1)
-          ),
-          ),
+          'Студак',
+          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1)),
+        ),
         backgroundColor: Colors.orange,
       ),
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color.fromRGBO(0, 0, 0, 1), Color.fromRGBO(0, 0, 0, 1)]
-          )
-        ),
-        padding: EdgeInsets.only(top:7),
+          color: Colors.white,
+        ), //, bottom: 8),
         child: ListView(
           children: <Widget>[
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 250.0,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
               ),
-              shadowColor: Colors.orange,
-              color: Colors.white,
+              items: [
+                createElemForCarousel(
+                    "Кафе и Рестораны", Colors.red, Colors.white),
+                createElemForCarousel("Антикафе", Colors.orange, Colors.white),
+                createElemForCarousel("Бары", Colors.green, Colors.white),
+                createElemForCarousel("Еще что-то", Colors.blue, Colors.white),
+              ],
+            ),
+            /*Text("Новинки",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20.0,
+              ),
+            ),*/
+            Container(
               child: ListTile(
-                onTap: () {
-                  debugPrint("I was clicked");
-                },
-                leading: Icon(
-                  Icons.accessibility,
-                  size: 56,
-                ),
-                title: Text('Two-line ListTile'),
-                subtitle: Text('Here is a second line'),
-                trailing: Icon(Icons.desktop_mac)
-              ),
+                  onTap: () {
+                    debugPrint("I was clicked");
+                  },
+                  leading: Icon(
+                    Icons.local_bar,
+                    size: 56,
+                  ),
+                  title: Text('Two-line ListTile'),
+                  subtitle: Text('Here is a second line'),
+                  trailing: Icon(Icons.fastfood)),
             ),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton.extended(
-        label: Text(
-          "Next Page",
-          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1)),
-        ),
-        icon: Icon(
-          Icons.arrow_forward,
-          color: Color.fromRGBO(0, 0, 0, 1),
-        ),
-        backgroundColor: Colors.orange,
-        onPressed: () {
-          Navigator.push(
-            context,
-            CupertinoPageRoute(builder: (context) => PageTwo()),
-          );
-        }
-      ),
+          label: Text(
+            "Favourite",
+            style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1)),
+          ),
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          backgroundColor: Colors.white,
+          onPressed: () {
+            Navigator.push(
+              context,
+              CupertinoPageRoute(builder: (context) => PageTwo()),
+            );
+          }),
     );
   }
 }
